4761d9c2d45c53174731a817306272ab7a0df9d0
Bruno <brunolealmaciel@gmail.com>
2023-10-22 18:33:21 -0300

    fixing e implementando coisas

--- src/pages/home/FilmesHome.jsx
+++ src/pages/home/FilmesHome.jsx
@@ -1,70 +1,89 @@
 import React, { useEffect, useState } from "react";
 import { Container, Movie, MovieList, Btn } from "../filmes/style";
 import { Link } from "react-router-dom";
-import {MdChevronLeft, MdChevronRight} from "react-icons/md"
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function FilmesHome() {
-    return (
-        <div>
-            <Filmes name='Terror' genero='27'/>
-            <Filmes name='Comédia' genero='35'/>
-            <Filmes name='Ação' genero='28'/>
-
-        </div>
-
-        
-    );
+  return (
+    <div>
+      <Filmes title="Terror" genero="27" />
+      <Filmes title="Ficção Científica" genero="878" />
+      <Filmes title="Comédia" genero="35" />
+      <Filmes title="Ação" genero="28" />
+      <Filmes title="Aventura" genero="12" />
+      <Filmes title="Animação" genero="16" />
+      <Filmes title="Drama" genero="18" />
+      <Filmes title="Romance" genero="10749" />
+    </div>
+  );
 }
 
-function Filmes({name, genero}){
-    const slideLeft = () => {
-        let slider = document.getElementById('slider')
-        slider.scrollLeft = slider.scrollLeft + 500
-    }
+function Filmes({ title, genero }) {
+  const sliderId = `slider-${genero}`;
+  const slideLeft = () => {
+    let slider = document.getElementById(sliderId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
-    const slideRight = () => {
-        let slider = document.getElementById('slider')
-        slider.scrollLeft = slider.scrollLeft - 500
-    }
-    const imagePath = "https://image.tmdb.org/t/p/original";
+  const slideRight = () => {
+    let slider = document.getElementById(sliderId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
-    const KEY = process.env.REACT_APP_KEY;
-    const [film, setFilm] = useState([]);
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=pt-BR&with_genres=${genero}&include_adult=false&include_video=false&sort_by=popularity.desc&vote_average.gte=0&vote_count.gte=1000
-        `)
-            .then((response) => response.json())
-            .then((data) => {
-                const terror = data.results.slice(10, 20);
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  const KEY = process.env.REACT_APP_KEY;
+  const [film, setFilm] = useState([]);
 
-                setFilm(terror);
-            });
-    }, [KEY]);
-    return(
-        <>
-               <div>
-            <h1 className=" text-3xl ps-7 font-semibold  ">{name}</h1>
-            <div className=" relative flex items-center" >
-                <MdChevronLeft className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideRight} size={40}/>
-                |<div id="slider" className="scrollbar-hide w-full h-full overflow-x-scroll  overflow-y-hidden scroll whitespace-nowrap scroll-smooth">     
-                    {film && film.length > 0 && film.map((film) => {
-                        return (
-                            <Link to={`/${film.id}`}>
-                            <div className=" pt-5 ps-7 w-[300px] h-[450px] pe-8 cursor-pointer hover:scale-105 ease-in-out duration-300  inline-block" key={film.id}>
-                                <img className=" w-full h-80 rounded-xl"
-                                    src={`${imagePath}${film.poster_path}`}
-                                    alt={film.title}
-                                />
-                                </div>
-                            </Link>
-                        );
-                    })}
-                </div>
-                <MdChevronRight className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideLeft} size={40}/>
-            </div >
-        </div> 
-        </>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=pt-BR&with_genres=${genero}&include_adult=false&include_video=false&sort_by=popularity.desc&vote_average.gte=0&vote_count.gte=1000`
+        );
+        const data = await response.json();
+        setFilm(data.results.slice(10, 20));
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, [genero]);
+
+  return (
+    <div>
+      <h1 className="text-3xl ps-7 font-semibold">{title}</h1>
+      <div className="relative flex items-center">
+        <MdChevronLeft
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+        />
+        |
+        <div
+          id={sliderId}
+          className="scrollbar-hide w-full h-full overflow-x-scroll overflow-y-hidden scroll whitespace-nowrap scroll-smooth"
+        >
+          {film && film.length > 0 && film.map((film) => (
+            <Link to={`/${film.id}`} key={film.id}>
+              <div className="pt-5 ps-7 w-[300px] h-[450px] pe-8 cursor-pointer hover:scale-105 ease-in-out duration-300 inline-block">
+                <img
+                  className="w-full h-80 rounded-xl"
+                  src={`${imagePath}${film.poster_path}`}
+                  alt={film.title}
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+        <MdChevronRight
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default FilmesHome;
