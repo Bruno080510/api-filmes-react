b268e6e3f6ec146910f8015a85e955765763709b
Bruno <brunolealmaciel@gmail.com>
2023-10-22 17:52:36 -0300

    deixando o código mais funcional e clean

--- src/pages/home/FilmesHome.jsx
+++ src/pages/home/FilmesHome.jsx
@@ -4,39 +4,19 @@ import { Link } from "react-router-dom";
 import {MdChevronLeft, MdChevronRight} from "react-icons/md"
 
 function FilmesHome() {
-    const hoje = new Date();
-    const dataAtual = hoje.toISOString().split('T')[0]; 
-    const proximaSemana = new Date(hoje);
-    proximaSemana.setDate(hoje.getDate() + 7); 
-    const dataProximaSemana = proximaSemana.toISOString().split('T')[0]; 
-    const number = 0
+    return (
+        <div>
+            <Filmes name='Terror' genero='27'/>
+            <Filmes name='Comédia' genero='35'/>
+            <Filmes name='Ação' genero='28'/>
 
-    const imagePath = "https://image.tmdb.org/t/p/original";
+        </div>
 
-    const [movies, setMovies] = useState([]);
-    const KEY = process.env.REACT_APP_KEY;
-    
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${dataAtual}&primary_release_date.lte=${dataProximaSemana}`)
-            .then((response) => response.json())
-            .then((data) => {
-                const top10Movies = data.results.slice(0, 10);
-                setMovies(top10Movies);
-            });
-    }, [KEY]);
+        
+    );
+}
 
-    const [film, setFilm] = useState([]);
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=pt-BR&with_genres=27&include_adult=false&include_video=false&sort_by=popularity.desc&vote_average.gte=0&vote_count.gte=1000
-        `)
-            .then((response) => response.json())
-            .then((data) => {
-                const terror = data.results.slice(10, 20);
-
-                setFilm(terror);
-            });
-    }, [KEY]);
-
+function Filmes({name, genero}){
     const slideLeft = () => {
         let slider = document.getElementById('slider')
         slider.scrollLeft = slider.scrollLeft + 500
@@ -46,11 +26,24 @@ function FilmesHome() {
         let slider = document.getElementById('slider')
         slider.scrollLeft = slider.scrollLeft - 500
     }
+    const imagePath = "https://image.tmdb.org/t/p/original";
 
-    return (
-        <div>
-            <div>
-            <h1 className=" text-3xl ps-7 font-semibold  ">Terror</h1>
+    const KEY = process.env.REACT_APP_KEY;
+    const [film, setFilm] = useState([]);
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=pt-BR&with_genres=${genero}&include_adult=false&include_video=false&sort_by=popularity.desc&vote_average.gte=0&vote_count.gte=1000
+        `)
+            .then((response) => response.json())
+            .then((data) => {
+                const terror = data.results.slice(10, 20);
+
+                setFilm(terror);
+            });
+    }, [KEY]);
+    return(
+        <>
+               <div>
+            <h1 className=" text-3xl ps-7 font-semibold  ">{name}</h1>
             <div className=" relative flex items-center" >
                 <MdChevronLeft className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideRight} size={40}/>
                 |<div id="slider" className="scrollbar-hide w-full h-full overflow-x-scroll  overflow-y-hidden scroll whitespace-nowrap scroll-smooth">     
@@ -69,32 +62,9 @@ function FilmesHome() {
                 </div>
                 <MdChevronRight className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideLeft} size={40}/>
             </div >
-        </div>
-
-
-<h1 className=" text-3xl ps-7 font-semibold  ">10s today</h1>
-<div className=" relative flex items-center" >
-    <MdChevronLeft className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideRight} size={40}/>
-    |<div id="slider" className="scrollbar-hide w-full h-full overflow-x-scroll  overflow-y-hidden scroll whitespace-nowrap scroll-smooth">     
-        {movies && movies.length > 0 && movies.map((movie) => {
-            return (
-                <Link to={`/${movie.id}`}>
-                <div className=" pt-5 ps-7 w-[300px] h-[450px] pe-8 cursor-pointer hover:scale-105 ease-in-out duration-300  inline-block" key={movie.id}>
-                    <img className=" w-full h-80 rounded-xl"
-                        src={`${imagePath}${movie.poster_path}`}
-                        alt={movie.title}
-                    />
-                    </div>
-                </Link>
-            );
-        })}
-    </div>
-    <MdChevronRight className="opacity-50 cursor-pointer hover:opacity-100" onClick={slideLeft} size={40}/>
-</div >
-        </div>
-
-        
-    );
+        </div> 
+        </>
+    )
 }
 
 export default FilmesHome;
